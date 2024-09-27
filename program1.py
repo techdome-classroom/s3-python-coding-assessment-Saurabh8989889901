3a3902697e7415db35e3acfbd43da27d4f44a923
Vivek Kumar Dhoke <ravishankar20041973@gmail.com>
2024-09-27 15:41:13 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -1,20 +1,15 @@
 class Solution(object):
     def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        pass
+        bracket_map = {')': '(', '}': '{', ']': '['}
+        stack = []
 
+        for char in s:
+            if char in bracket_map.values():
+                stack.append(char)
+            elif char in bracket_map.keys():
+                if stack and stack[-1] == bracket_map[char]:
+                    stack.pop()
+                else:
+                    return False
 
-
-
-
-
-
-    
-
-
-
-  
-
+        return not stack
